f8fb0fb9966a51bd331e598b6d436b13ce20f85c
Dmitry Prokopenko <113458878+micromoleckula@users.noreply.github.com>
2023-09-02 23:32:21 +0300

    02.09.2023

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 2,
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
+  
+  });
